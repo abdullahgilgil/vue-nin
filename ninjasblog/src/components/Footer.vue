4b4f47eb9d84efc2dff578e277fe629138bf1a37
Abdullah Gilgil <abdullah_gilgil@hotmail.com>
2019-01-28 17:01:40 +0000

    24

---
+++ ninjasblog/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+   <footer>
+      <p>{{ copyright }} {{title}}</p>
+   </footer>
+</template>
+
+<script>
+export default {
+   props: {
+      title : {
+         type: String
+      }
+   },
+  data() {
+    return {
+       copyright : "Created at home - 2019 - by "
+    }
+  }
+}
+</script>
+
+<style scoped>
+   footer {
+      background-color: #222;
+      margin-top: 224px;
+   }
+   p {
+      padding: 12px 24px;
+      text-align: center;
+      font-size: 24px;
+      color: lightgreen;
+   }
+
+</style>
