4960d8d9a84822e476e3a7bf9df3a0bfab072489
Abdullah Gilgil <abdullah_gilgil@hotmail.com>
2019-01-29 14:49:22 +0000

    les-36 filters

--- ninjasblog/src/App.vue
+++ ninjasblog/src/App.vue
@@ -1,38 +1,30 @@
 <template>
   <div>
-    <keep-alive>
-      <component v-bind:is="component"> </component>
-    </keep-alive>  
-    <button v-on:click="handleFormNumberOne">Show One</button>
-    <button v-on:click="handleFormNumbertwo">Show Two</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import formOne from './components/formOne';
-import formTwo from './components/formTwo';
+import addBlog from './components/addBlog.vue'
 
 export default {
   components : {
-    'form-one' : formOne,
-    'form-two' : formTwo
-  },
+      'add-blog': addBlog
+    },
   data() {
+    
     return {
-      component : 'form-one'
+      
     }
   },
   methods: {
-    handleFormNumberOne: function(){
-      this.component = 'form-one'
-    },
-    handleFormNumbertwo: function(){
-      this.component = 'form-two'
-    }
   }
 }
 </script>
 
 <style>
-  
+  body{
+      margin: 0;
+      font-family: 'Nunito SemiBold';
+   }
 </style>
---
+++ ninjasblog/src/components/addBlog.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="add-blog">
+    <h2>Add a new blog post</h2>
+    <form>
+      <label for="">Blog Title:</label>
+      <input v-model="blog.title" type="text" required>
+      <label for="">Blog Content:</label>
+      <textarea v-model.lazy="blog.content" v-on:keyup="handleCounter"></textarea>
+      <span>{{blog.counter}}</span>
+    </form>
+    <div id="preview">
+      <h3>Preview blog</h3>
+      <p>Blog title: {{blog.title}}</p>
+      <p>Blog content:</p>
+      <p style="white-space: pre">{{blog.content}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      blog : {
+        title : "",
+        content: "",
+        counter : 100
+      }
+    }
+  },
+  methods: {
+    handleCounter : function(){
+      let textarea = document.querySelector("textarea");
+      if(textarea.value !== ""){
+        // console.log(textarea.value.length);
+        this.blog.counter --;
+      }
+    
+    }
+  }
+}
+</script>
+
+<style>
+  #add-blog *{
+    box-sizing: border-box;
+}
+#add-blog{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+h3{
+    margin-top: 10px;
+}
+</style>
