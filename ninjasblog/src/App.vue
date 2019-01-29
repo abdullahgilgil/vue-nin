884d2524c84d3e7fb4191df29df24db3501144a8
Abdullah Gilgil <abdullah_gilgil@hotmail.com>
2019-01-29 13:00:12 +0000

    les-28, next-input binding

--- ninjasblog/src/App.vue
+++ ninjasblog/src/App.vue
@@ -1,49 +1,34 @@
 <template>
-<div>
-  <form-helper>
-    <h2 slot="title">{{ title }}</h2>
-    <p slot="desc">I am a slot paragraph</p>
-  </form-helper>
-  <hr>
-  <app-form>
-    <div slot="form-header">
-      <h3>This is title of the form</h3>
-      <p>Information about the form</p>
-    </div>
-
-    <div slot="form-fields">
-      <div>
-        <label for="">Name</label>
-        <input type="text" placeholder="Name" required>
-      </div>
-      <div>
-        <label for="">Password</label>
-        <input type="password" placeholder="Password" required>
-      </div>
-    </div>
-
-    <div slot="form-controls">
-      <button v-on:click='handleSubmit'>Submit</button>
-    </div>
-  </app-form>
-</div>
+  <div>
+    <keep-alive>
+      <component v-bind:is="component"> </component>
+    </keep-alive>  
+    <button v-on:click="handleFormNumberOne">Show One</button>
+    <button v-on:click="handleFormNumbertwo">Show Two</button>
+  </div>
 </template>
 
 <script>
-import formHelper from './components/formHelper.vue';
-import Form from './components/Form'
+import formOne from './components/formOne';
+import formTwo from './components/formTwo';
+
 export default {
   components : {
-    'form-helper' : formHelper,
-    'app-form' : Form
+    'form-one' : formOne,
+    'form-two' : formTwo
   },
   data() {
     return {
-      title : "I am a dynamic slot header from data"
+      component : 'form-one'
     }
   },
   methods: {
-
+    handleFormNumberOne: function(){
+      this.component = 'form-one'
+    },
+    handleFormNumbertwo: function(){
+      this.component = 'form-two'
+    }
   }
 }
 </script>
